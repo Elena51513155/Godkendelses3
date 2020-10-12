247ccf5b85ef2320874e7d4a1d7c5c8990b0d877
Elena Marie Frees <elenamariefrees@Elenas-MacBook-Air.local>
2020-10-12 13:17:02 +0200

    first commit

---
+++ server3.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+// default URL to API
+app.use('/', function(req, res) {
+    res.send('Succes');
+});
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+
+console.debug('Server listening on port ' + port);
+# Godkendelses3
